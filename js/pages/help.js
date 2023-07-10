9764378a9f6c00dcfcbd7980dd9f6841edbc9902
MiltonOscarFlores <miltonoscarflores@gmail.com>
2023-07-10 01:56:33 -0300

    Listo el help.html - falta favoritos.html

--- js/pages/help.js
+++ js/pages/help.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
+
     const form = document.querySelector("form");
     const nombreInput = document.querySelector("#nombre");
     const emailInput = document.querySelector("#email");
@@ -6,39 +7,50 @@ document.addEventListener("DOMContentLoaded", () => {
     const errorNombre = document.querySelector("#nombreError");
     const errorEmail = document.querySelector("#emailError");
     const errorMensaje = document.querySelector("#errorMensaje");
-  
+    const limpiar = document.querySelector("#limpiar")
+    const enviado = document.querySelector("#enviado")
+    
     form.addEventListener("submit", (event) => {
-      event.preventDefault();
-  
-      const nombreValue = nombreInput.value.trim();
-      const emailValue = emailInput.value.trim();
-      const mensajeValue = mensajeInput.value.trim();
-  
-      if (nombreValue === "") {
-        errorNombre.textContent = "Debe ingresar un nombre";
-        errorNombre.style.display = "block";
-      } else {
-        errorNombre.style.display = "none";
-      }
-  
-      if (emailValue === "") {
-        errorEmail.textContent = "Debe ingresar un email";
-        errorEmail.style.display = "block";
-      } else {
-        errorEmail.style.display = "none";
-      }
-  
-      if (mensajeValue === "") {
-        errorMensaje.textContent = "Debe ingresar un mensaje";
-        errorMensaje.style.display = "block";
-      } else {
-        errorMensaje.style.display = "none";
-      }
-  
-      if (nombreValue !== "" && emailValue !== "" && mensajeValue !== "") {
-        // Aquí puedes realizar cualquier acción adicional que desees al enviar el formulario
-        form.reset();
-      }
+        event.preventDefault();
+        
+        const nombreValue = nombreInput.value.trim();
+        const emailValue = emailInput.value.trim();
+        const mensajeValue = mensajeInput.value.trim();
+
+        if (nombreValue === "") {
+            errorNombre.textContent = "Debe ingresar un nombre";
+            errorNombre.style.display = "block";
+        } else {
+            errorNombre.style.display = "none";
+        }
+
+        if (emailValue === "") {
+            errorEmail.textContent = "Debe ingresar un email";
+            errorEmail.style.display = "block";
+        } else {
+            errorEmail.style.display = "none";
+        }
+
+        if (mensajeValue === "") {
+            errorMensaje.textContent = "Debe ingresar un mensaje";
+            errorMensaje.style.display = "block";
+        } else {
+            errorMensaje.style.display = "none";
+        }
+
+        if (nombreValue !== "" && emailValue !== "" && mensajeValue !== "") {
+            // Aquí puedes realizar cualquier acción adicional que desees al enviar el formulario
+            enviado.textContent = "Enviado Exitosamente"
+            enviado.style.display = "block"
+            form.reset();
+            setTimeout(() => {
+                enviado.style.display = "none"
+            }, 2500);
+        }
+
     });
-  });
-  
+
+    limpiar.addEventListener("click", () => {
+        form.reset()
+    })
+});
