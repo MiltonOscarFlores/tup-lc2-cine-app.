64b3fcac43fa7c541112b7bf876d43a33e51e4df
MiltonOscarFlores <miltonoscarflores@gmail.com>
2023-07-14 03:19:39 -0300

    Proyecto TERMINADO (solo faltarian los req extras)

--- js/common.js
+++ js/common.js
@@ -1,70 +1,144 @@
 document.addEventListener("DOMContentLoaded", async () => {
+  // REFERENCIAS
+  let pagina = 1;
+  const btnAnterior = document.querySelector("#btnAnterior");
+  const btnSiguiente = document.querySelector("#btnSiguiente");
+  const paginaActual = document.querySelector("#paginaActual");
 
-    // REFERENCIAS
-    let pagina = 1;
-    const btnAnterior = document.querySelector("#btnAnterior");
-    const btnSiguiente = document.querySelector("#btnSiguiente");
-    const paginaActual = document.querySelector("#paginaActual");
+  // PELICULAS
+  const cargarEstrenos = async () => {
+    try {
+      let respuesta = await fetch(
+        `https://api.themoviedb.org/3/movie/now_playing?api_key=d499f83b1fc4e52d8070293d3520f049&Language=es-AR&page=${pagina}`
+      );
 
-    // PELICULAS
-    const cargarEstrenos = async () => {
-      try {
-        let respuesta = await fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=d499f83b1fc4e52d8070293d3520f049&Language=es-AR&page=${pagina}`);
+      if (respuesta.status === 200) {
+        let datos = await respuesta.json();
 
-        if (respuesta.status === 200) {
-          let datos = await respuesta.json();
+        const contenedorPeliculas = document.querySelector("#sec_peliculas");
+        contenedorPeliculas.innerHTML = ""; // Limpio el contenedor de peliculas
 
-          const contenedorPeliculas = document.querySelector("#sec_peliculas");
-          contenedorPeliculas.innerHTML = ""; // Limpio el contenedor de peliculas
+        datos.results.forEach((pelicula) => {
+          const peliculaId = pelicula.id;
+          const esFavorito = esPeliculaFavorita(peliculaId);
 
-          datos.results.forEach(pelicula => {
-            contenedorPeliculas.innerHTML += `<div class="contenedorPeliculas">
+          contenedorPeliculas.innerHTML += `<div class="contenedorPeliculas" data-id="${peliculaId}">
               <img src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}" alt="creed" />
               <h3>${pelicula.title}</h3>
               <p>
-                <b>Código:</b> ${pelicula.id} <br>
+                <b>Código:</b> ${peliculaId} <br>
                 <b>Título original:</b> ${pelicula.original_title} <br>
                 <b>Idioma original:</b> ${pelicula.original_language} <br>
                 <b>Año:</b> ${pelicula.release_date}
               </p>
               <div class="padreboton">
-                <button type="submit" class="button">Agregar a Favoritos</button>
+                <button type="button" class="button ${esFavorito ? 'btn-quitar-favoritos' : 'btn-agregar-favoritos'}">
+                  ${esFavorito ? 'Quitar de Favoritos' : 'Agregar a Favoritos'}
+                </button>
               </div>
             </div>`;
+        });
+
+        // Verificar si cada película está en la lista de favoritos y actualizar el botón en consecuencia
+        const peliculas = contenedorPeliculas.querySelectorAll(".contenedorPeliculas");
+        peliculas.forEach((pelicula) => {
+          const peliculaId = pelicula.dataset.id;
+          const boton = pelicula.querySelector(".button");
+
+          if (esPeliculaFavorita(peliculaId)) {
+            boton.textContent = "Quitar de Favoritos";
+            boton.classList.remove("btn-agregar-favoritos");
+            boton.classList.add("btn-quitar-favoritos");
+          }
+        });
+
+        paginaActual.textContent = pagina;
+
+        // Agregar controlador de eventos al botón "Agregar a Favoritos" o "Quitar de Favoritos" de cada película
+        const favoritosButtons = document.querySelectorAll(".button");
+        favoritosButtons.forEach((button) => {
+          button.addEventListener("click", () => {
+            const peliculaElement = button.closest(".contenedorPeliculas");
+            const peliculaId = peliculaElement.dataset.id;
+
+            if (button.classList.contains("btn-agregar-favoritos")) {
+              // Agregar la película a favoritos
+              agregarAFavoritos(peliculaId);
+
+              // Actualizar el botón a "Quitar de Favoritos"
+              button.textContent = "Quitar de Favoritos";
+              button.classList.remove("btn-agregar-favoritos");
+              button.classList.add("btn-quitar-favoritos");
+            } else if (button.classList.contains("btn-quitar-favoritos")) {
+              // Quitar la película de favoritos
+              quitarDeFavoritos(peliculaId);
+
+              // Actualizar el botón a "Agregar a Favoritos"
+              button.textContent = "Agregar a Favoritos";
+              button.classList.remove("btn-quitar-favoritos");
+              button.classList.add("btn-agregar-favoritos");
+            }
           });
-
-          paginaActual.textContent = pagina;
-        } else if (respuesta.status === 401) {
-          console.log("Error 401 de autenticación clave API");
-        } else if (respuesta.status === 404) {
-          console.log("Error 404: La película no existe");
-        } else {
-          console.log("Ocurrió un error desconocido");
-        }
-      } catch (error) {
-        console.log(error);
+        });
+      } else if (respuesta.status === 401) {
+        console.log("Error 401 de autenticación clave API");
+      } else if (respuesta.status === 404) {
+        console.log("Error 404: La película no existe");
+      } else {
+        console.log("Ocurrió un error desconocido");
       }
-    };
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-    cargarEstrenos();
+  cargarEstrenos();
 
+  // Función para verificar si una película está en la lista de favoritos
+  const esPeliculaFavorita = (peliculaId) => {
+    let codigosPeliculas = localStorage.getItem("codigosPeliculas");
+    codigosPeliculas = codigosPeliculas ? JSON.parse(codigosPeliculas) : [];
+    return codigosPeliculas.includes(peliculaId);
+  };
 
+  // Función para agregar película a Favoritos en el almacenamiento local
+  const agregarAFavoritos = (peliculaId) => {
+    let codigosPeliculas = localStorage.getItem("codigosPeliculas");
+    codigosPeliculas = codigosPeliculas ? JSON.parse(codigosPeliculas) : [];
 
-    // EVENTOS DE PAGINACION
+    // Verificar si la película ya está en la lista de Favoritos
+    if (!codigosPeliculas.includes(peliculaId)) {
+      codigosPeliculas.push(peliculaId);
+      localStorage.setItem("codigosPeliculas", JSON.stringify(codigosPeliculas));
+    }
+  };
 
-    btnSiguiente.addEventListener("click", () => {
-      if (pagina < 1000) {
-        pagina += 1;
-        cargarEstrenos();
-        paginaActual.textContent = pagina;
-      }
-    });
+  // Función para quitar película de Favoritos en el almacenamiento local
+  const quitarDeFavoritos = (peliculaId) => {
+    let codigosPeliculas = localStorage.getItem("codigosPeliculas");
+    codigosPeliculas = codigosPeliculas ? JSON.parse(codigosPeliculas) : [];
 
-    btnAnterior.addEventListener("click", () => {
-      if (pagina > 1) {
-        pagina -= 1;
-        cargarEstrenos();
-        paginaActual.textContent = pagina;
-      }
-    });
+    // Filtrar el array para crear un nuevo array sin el código de película que se quiere eliminar
+    const nuevoArray = codigosPeliculas.filter((codigo) => codigo !== peliculaId);
+
+    // Guardar el nuevo array en el almacenamiento local
+    localStorage.setItem("codigosPeliculas", JSON.stringify(nuevoArray));
+  };
+
+  // EVENTOS DE PAGINACION
+  btnSiguiente.addEventListener("click", () => {
+    if (pagina < 1000) {
+      pagina += 1;
+      cargarEstrenos();
+      paginaActual.textContent = pagina;
+    }
+  });
+
+  btnAnterior.addEventListener("click", () => {
+    if (pagina > 1) {
+      pagina -= 1;
+      cargarEstrenos();
+      paginaActual.textContent = pagina;
+    }
+  });
 });
--- js/pages/favorities.js
+++ js/pages/favorities.js
@@ -1,23 +1,22 @@
-document.addEventListener("DOMContentLoaded", async () => {
+document.addEventListener("DOMContentLoaded", () => {
+    const contenedorPeliculas = document.querySelector("#sec-favorities-list");
+    const mensajeWarning = document.querySelector("#warningFavoritos");
 
     const cargarFavoritos = async () => {
-        const contenedorPeliculas = document.querySelector("#sec-favorities-list");
-        const mensajeWarning = document.querySelector("#warningFavoritos");
-
-        // Obtengo el array de códigos desde el localStorage
+        // Obtener el array de códigos desde el localStorage
         let codigosPeliculas = localStorage.getItem("codigosPeliculas");
         codigosPeliculas = codigosPeliculas ? JSON.parse(codigosPeliculas) : [];
 
         if (codigosPeliculas.length === 0) {
             // No hay películas seleccionadas, mostrar el mensaje de advertencia
             mensajeWarning.style.display = "block";
-            
         } else {
             // Hay películas seleccionadas, ocultar el mensaje de advertencia
             mensajeWarning.style.display = "none";
 
             for (let codigo of codigosPeliculas) {
                 try {
+                    // Obtener los detalles de la película desde la API
                     let respuesta = await fetch(
                         `https://api.themoviedb.org/3/movie/${codigo}?api_key=d499f83b1fc4e52d8070293d3520f049&Language=es-AR`
                     );
@@ -25,22 +24,20 @@ document.addEventListener("DOMContentLoaded", async () => {
                     if (respuesta.status === 200) {
                         let pelicula = await respuesta.json();
 
-                        console.log(pelicula);
-
                         contenedorPeliculas.innerHTML += `
-              <div class="contenedorPeliculasFavoritas">
-                <img src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}" alt="poster" />
-                <h3>${pelicula.title}</h3>
-                <p>
-                  <b>Código:</b> ${pelicula.id} <br>
-                  <b>Título original:</b> ${pelicula.original_title} <br>
-                  <b>Idioma original:</b> ${pelicula.original_language} <br>
-                  <b>Resumen:</b> ${pelicula.overview}
-                </p>
-                <div class="padreboton">
-                  <button type="submit" class="button">Quitar de Favoritos</button>
-                </div>
-              </div>`;
+                <div class="contenedorPeliculasFavoritas" data-id="${pelicula.id}">
+                  <img src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}" alt="poster" />
+                  <h3>${pelicula.title}</h3>
+                  <p>
+                    <b>Código:</b> ${pelicula.id} <br>
+                    <b>Título original:</b> ${pelicula.original_title} <br>
+                    <b>Idioma original:</b> ${pelicula.original_language} <br>
+                    <b>Resumen:</b> ${pelicula.overview}
+                  </p>
+                  <div class="padreboton">
+                    <button type="button" class="button btn-quitar">Quitar de Favoritos</button>
+                  </div>
+                </div>`;
                     } else if (respuesta.status === 401) {
                         console.log("Error 401 de autenticación clave API");
                     } else if (respuesta.status === 404) {
@@ -56,5 +53,53 @@ document.addEventListener("DOMContentLoaded", async () => {
     };
 
     cargarFavoritos();
+
+    contenedorPeliculas.addEventListener("click", (event) => {
+        // Verificar si se hizo clic en el botón "Quitar de Favoritos"
+        if (event.target.classList.contains("btn-quitar")) {
+            const peliculaElement = event.target.closest(".contenedorPeliculasFavoritas");
+            const peliculaId = peliculaElement.dataset.id;
+
+            // Llamar a la función para eliminar la película del almacenamiento local y la interfaz de usuario
+            quitarDeFavoritos(peliculaId);
+
+            // Eliminar la película del DOM
+            peliculaElement.remove();
+
+            console.log("pelicula acabo de borrar ID: " + peliculaId);
+        }
+    });
+
+    const quitarDeFavoritos = (peliculaId) => {
+        // Obtener el array de códigos de películas desde el almacenamiento local
+        let codigosPeliculas = localStorage.getItem("codigosPeliculas");
+        codigosPeliculas = codigosPeliculas ? JSON.parse(codigosPeliculas) : [];
+        console.log("Array del localstorage: " + codigosPeliculas);
+      
+        // Convertir los elementos del array a números
+        codigosPeliculas = codigosPeliculas.map((codigo) => Number(codigo));
+      
+        // Encontrar el índice del código de película que se quiere eliminar
+        const indice = codigosPeliculas.indexOf(Number(peliculaId));
+      
+        // Si se encontró el código de película, eliminarlo del array
+        if (indice !== -1) {
+          codigosPeliculas.splice(indice, 1);
+        }
+      
+        // Convertir los elementos del array nuevamente a strings
+        codigosPeliculas = codigosPeliculas.map((codigo) => String(codigo));
+      
+        // Guardar el nuevo array en el almacenamiento local
+        localStorage.setItem("codigosPeliculas", JSON.stringify(codigosPeliculas));
+        console.log("Nuevo array: " + JSON.stringify(codigosPeliculas));
+      };
+      
+      
+      
+
+
+
+
 
 });
